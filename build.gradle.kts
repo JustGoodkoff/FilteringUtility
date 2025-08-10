f8335d4504a453b5927872c004b0e482635df792
ANDREY GUDKOV <a.gudkov@postgrespro.ru>
2025-08-10 22:07:20 +0700

    add shadowjar to build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    application
+    id("com.gradleup.shadow") version "9.0.0-rc3"
 }
 
 group = "org.example"
@@ -13,6 +15,16 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("commons-cli:commons-cli:1.9.0")
+}
+
+application {
+    mainClass.set("ru.nsu.agudkov.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "ru.nsu.agudkov.Main"
+    }
 }
 
 tasks.test {
